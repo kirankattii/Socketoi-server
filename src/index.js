a5a17c2943ab8131bc8f5df851fcd3381a6639ba
kiran katti <kirankatti550@gmail.com>
2024-12-21 15:01:45 +0530

    add cors updated

--- src/index.js
+++ src/index.js
@@ -15,8 +15,19 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
   origin: "https://socketio-client-azure.vercel.app",
-  credentials: true
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed HTTP methods
+  allowedHeaders: "Content-Type,Authorization" // Allowed headers
+
 }))
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://socketio-client-azure.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send("Wokring")
